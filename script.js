bc63da40bdad1ee42c81f4a1c8932d2731db641d
Navya <88023981+navysai1419@users.noreply.github.com>
2024-01-24 21:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getFormvalue() {
 	var form =document.getElementById("form1");
 	let firstName=form.elements['fname'].value;
 	let lastName=form.elements['lname'].value;
-	alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
+	alert(firstName+''+lastName);
 	
 return false
 }
